ce5e40f71cb7e71cffdcf95bcb0179f77adcb954
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-07-27 09:33:24 +0700

    update home-news

--- src/components/home/News.js
+++ src/components/home/News.js
@@ -1,35 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 let news = [
     {
         img: "./img/newimg.jpg",
         title: "1Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+        path: "/article"
     },
     {
         img: "./img/newimg.jpg",
         title: "2Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+        path: "/article"
     },
     {
         img: "./img/newimg.jpg",
         title: "3Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+        path: "/article"
     },
     {
         img: "./img/newimg.jpg",
         title: "4Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+        path: "/article"
     },
     {
         img: "./img/newimg.jpg",
         title: "5Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+        path: "/article"
     },
     {
         img: "./img/newimg.jpg",
         title: "6Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor",
-        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua..."
+        desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...",
+        path: "/article"
     }
 ]
 
@@ -37,12 +44,18 @@ let newsDisplay = news.slice(news.length - 4, news.length)
 newsDisplay.reverse()
 
 function BigNew(props) {
-    const { img, title, desc } = props
+    const { img, title, desc, path } = props
 
     return (
         <div className="w-[39rem] border-r-[0.063rem] border-[#e9e9e9] absolute">
-            <img className="w-[38rem] h-[21.375rem]" src={img} alt="news" />
-            <h2 className="text-[1.546rem] font-bold text-primary mt-8 mb-4">{title}</h2>
+            <Link to={path}>
+                <div className="w-[38rem] h-[21.375rem]">
+                    <img className="w-[38rem] h-[21.375rem]" src={img} alt="news" />
+                </div>
+            </Link>
+            <div className="mt-8 mb-4">
+                <Link className="text-[1.546rem] font-bold text-primary" to={path}>{title}</Link>
+            </div>
             <p className="text-[1.203rem] text-gray">{desc}</p>
         </div>
 
@@ -50,14 +63,18 @@ function BigNew(props) {
 }
 
 function SmallNew(props) {
-    const { img, title, desc } = props
+    const { img, title, desc, path } = props
 
     return (
         <div className="border-b-[0.063rem] border-[#e9e9e9] mb-[1.875rem] ml-[39.938rem]">
             <div className="w-[35.063rem] flex flex-row mb-[0.938rem]">
-                <img className="w-48 h-[6.75rem]" src={img} alt="news" />
+                <Link to={path}>
+                    <div className="w-48 h-[6.75rem]">
+                        <img className="w-48 h-[6.75rem]" src={img} alt="news" />
+                    </div>
+                </Link>
                 <div className="ml-[0.938rem]">
-                    <h2 className="text-[0.915rem] font-bold text-primary mb-3">{title}</h2>
+                    <Link className="text-[0.915rem] font-bold text-primary mb-3" to={path}>{title}</Link>
                     <p className="text-[0.711rem] text-gray">{desc}</p>
                 </div>
             </div>
@@ -72,20 +89,22 @@ export function News() {
         <div className="h-[47.75rem]">
             <div className="text-center w-[75rem] grid grid-cols-[1fr_6.25rem] mx-auto mt-20">
                 <span className="text-primary font-bold text-5xl pl-[6.25rem]">TIN TỨC</span>
-                <a className="text-primary text-xl my-auto" href="#">Xem thêm &gt;</a>
+                <div className="my-auto">
+                    <Link className="text-primary text-xl" to="/tin-tuc">Xem thêm &gt;</Link>
+                </div>
             </div>
             <img className="mx-auto mb-[1.75rem]" src="./img/Website---eBacsi---final---cut_17.jpg" alt="." />
             <div className="w-[75rem] mx-auto mb-24">
-                {           
+                {
                     newsDisplay.map((item, index) => {
                         if (index % 4 === 0) {
                             return (
-                                <BigNew key={index} img={item.img} title={item.title} desc={item.desc} />
+                                <BigNew key={index} img={item.img} title={item.title} desc={item.desc} path={item.path} />
                             )
                         }
                         else {
                             return (
-                                <SmallNew key={index} img={item.img} title={item.title} desc={item.desc} />
+                                <SmallNew key={index} img={item.img} title={item.title} desc={item.desc} path={item.path} />
                             )
                         }
                     })
