40b0fba39de32703fb1c7a4aa1abc98872053969
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-07-31 17:40:14 +0700

    responsive home-news

--- src/components/home/News.js
+++ src/components/home/News.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 
 let news = [
@@ -47,10 +48,10 @@ function BigNew(props) {
     const { img, title, desc, path } = props
 
     return (
-        <div className="w-[39rem] border-r-[0.063rem] border-[#e9e9e9] absolute">
+        <div className="w-[39rem] border-r-[0.063rem] border-[#e9e9e9] absolute xl:w-[33rem]">
             <Link to={path}>
                 <div className="w-[38rem] h-[21.375rem]">
-                    <img className="w-[38rem] h-[21.375rem]" src={img} alt="news" />
+                    <img className="w-[38rem] h-[21.375rem] xl:w-[32rem]" src={img} alt="news" />
                 </div>
             </Link>
             <div className="mt-8 mb-4">
@@ -65,39 +66,84 @@ function BigNew(props) {
 function SmallNew(props) {
     const { img, title, desc, path } = props
 
-    return (
-        <div className="border-b-[0.063rem] border-[#e9e9e9] mb-[1.875rem] ml-[39.938rem]">
-            <div className="w-[35.063rem] flex flex-row mb-[0.938rem]">
-                <Link to={path}>
-                    <div className="w-48 h-[6.75rem]">
-                        <img className="w-48 h-[6.75rem]" src={img} alt="news" />
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
+    if (windowWidth >= 980) {
+        return (
+            <div className="border-b-[0.063rem] border-[#e9e9e9] mb-[1.875rem] ml-[39.938rem] xl:w-auto xl:ml-[33.938rem]">
+                <div className="w-[35.063rem] flex flex-row mb-[0.938rem] xl:w-auto">
+                    <Link to={path}>
+                        <div className="w-48 h-[6.75rem]">
+                            <img className="w-48 h-[6.75rem]" src={img} alt="news" />
+                        </div>
+                    </Link>
+                    <div className="ml-[0.938rem]">
+                        <Link className="text-[0.915rem] font-bold text-primary mb-3" to={path}>{title}</Link>
+                        <p className="text-[0.711rem] text-gray">{desc}</p>
                     </div>
-                </Link>
-                <div className="ml-[0.938rem]">
-                    <Link className="text-[0.915rem] font-bold text-primary mb-3" to={path}>{title}</Link>
-                    <p className="text-[0.711rem] text-gray">{desc}</p>
                 </div>
             </div>
-        </div>
-
-    )
+        )
+    }
+    else {
+        return (
+            <div className="border-b-[0.063rem] border-[#e9e9e9] mb-[1.875rem]">
+                <div className="w-[35.063rem] flex flex-row mb-[0.938rem] xl:w-auto ">
+                    <Link to={path}>
+                        <div className="w-48 h-[6.75rem] sm:w-auto sm:h-auto">
+                            <img className="w-48 h-[6.75rem] sm:w-auto sm:h-auto" src={img} alt="news" />
+                        </div>
+                    </Link>
+                    <div className="ml-[0.938rem] ">
+                        <Link className="text-[0.915rem] font-bold text-primary mb-3" to={path}>{title}</Link>
+                        <p className="text-[0.711rem] text-gray">{desc}</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
+
 
 export function News() {
 
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWindowWidth(window.innerWidth);
+    };
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize);
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div className="h-[47.75rem]">
-            <div className="text-center w-[75rem] grid grid-cols-[1fr_6.25rem] mx-auto mt-20">
+        <div className="pb-[5.625rem] xl:px-4">
+            <div className="text-center w-[75rem] grid grid-cols-[1fr_6.25rem] mx-auto mt-20 xl:w-auto">
                 <span className="text-primary font-bold text-5xl pl-[6.25rem]">TIN TỨC</span>
                 <div className="my-auto">
                     <Link className="text-primary text-xl" to="/tin-tuc">Xem thêm &gt;</Link>
                 </div>
             </div>
             <img className="mx-auto mb-[1.75rem]" src="./img/Website---eBacsi---final---cut_17.jpg" alt="." />
-            <div className="w-[75rem] mx-auto mb-24">
+            <div className="w-[75rem] mx-auto mb-24 xl:w-auto">
                 {
                     newsDisplay.map((item, index) => {
-                        if (index % 4 === 0) {
+                        if (index % 4 === 0 && windowWidth >= 980) {
                             return (
                                 <BigNew key={index} img={item.img} title={item.title} desc={item.desc} path={item.path} />
                             )
@@ -112,6 +158,5 @@ export function News() {
                 }
             </div>
         </div>
-
     )
 }
