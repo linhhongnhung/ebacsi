ef3f89ac763aea463e1ba7ed75475f74f17c2437
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-08-03 02:16:14 +0700

    add link to news title

--- src/components/news/Topic.js
+++ src/components/news/Topic.js
@@ -10,7 +10,7 @@ export function Topic(props) {
     return (
         <form className="flex flex-col">
             <div className="w-[75rem] h-[4.188rem] bg-primary text-center grid grid-cols-[1fr_11.875rem] mx-auto xl:w-full lg:h-10 lg:grid-cols-1">
-                <Link className="my-auto text-white font-bold text-4xl pl-[11.875rem] lg:text-[1.3rem] lg:pl-0">{topicName}</Link>
+                <Link className="my-auto text-white font-bold text-4xl pl-[11.875rem] lg:text-[1.3rem] lg:pl-0" to={path}>{topicName}</Link>
                 <Link className="my-auto text-2xl text-white lg:hidden" to={path}>Xem thêm »</Link>
             </div>
             {
