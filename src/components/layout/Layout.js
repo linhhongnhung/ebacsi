6662c682e6463985c2739bae53e609ad14762e53
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-08-03 21:54:56 +0700

    responsive 4 contact buttons

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
     return (
         <div className="relative">
             <div className="flex flex-row">
-                <div className="fixed z-[2] bottom-2 left-6 flex flex-row">
+                <div className="fixed z-[2] bottom-2 left-6 flex flex-row sm:flex-col sm:w-36">
                     <Link to="/">
                         <img src="./img/hotline.png" />
                     </Link>
@@ -13,7 +13,7 @@ const Layout = ({ children }) => {
                         <img src="./img/Website---eBacsi---final---cut_14.png" />
                     </Link>
                 </div>
-                <div className="fixed z-[2] bottom-4 right-6">
+                <div className="fixed z-[2] bottom-4 right-6 sm:w-14">
                     <Link  to="/">
                         <img src="./img/Website---eBacsi---final---cut_07.png" />
                     </Link>
