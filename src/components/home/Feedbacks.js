8c82894586aeb80f116a94cd56e98ca98b757c08
Linh Hong Nhung <linhhongnhung11@gmail.com>
2023-08-04 13:53:13 +0700

    add rating star

--- src/components/home/Feedbacks.js
+++ src/components/home/Feedbacks.js
@@ -16,11 +16,26 @@ function QrCode() {
 }
 
 function Rating(props) {
-    return (
-        <div>
 
-        </div>
-    )
+    const stars = [];
+
+    for (let i = 1; i <= 5; i++) {
+        if (i <= props.rating) {
+            stars.push(<img className="w-[1.25rem] mx-1" src="./img/feedbacks/star_full.png" alt="full star"/>)
+        }
+        else if (props.rating % 1 !== 0) {
+            stars.push(<img className="w-[1.25rem] mx-1" src="./img/feedbacks/star_half.png" alt="half star"/>)
+            for (let j = 0; j < 5-i; i++) {
+                stars.push(<img className="w-[1.25rem] mx-1" src="./img/feedbacks/star_empty.png" alt="empty star"/>)
+            }
+            break;
+        }
+        else {
+            stars.push(<img className="w-[1.25rem] mx-1" src="./img/feedbacks/star_empty.png" alt="empty star"/>)
+        }
+    }
+
+    return <div className="flex flex-row justify-center pb-9 mt-6">{stars}</div>
 }
 
 export function Feedbacks() {
@@ -43,7 +58,7 @@ export function Feedbacks() {
             <div className="bg-[url('../public/img/Banner4.jpg')] bg-cover w-full relative h-[63.125rem] mt-[17.25rem] xl:h-full">
                 <div className="w-[62.75rem] mb-32 pt-[27.5rem] mx-auto relative xl:w-auto xl:px-8 xl:pt-[52rem] xl:pb-16">
                     <Slider {...settings}>
-                        {feedbacks.map(feedback => {
+                        {feedbacks.map((feedback, index) => {
                             return (
                                 <div className="mx-auto rounded-[1rem] text-center bg-gradient-to-b from-[#f68b1f] to-primary p-[0.32rem]">
                                     <div className="bg-white px-[4.375rem] rounded-[0.8rem] md:px-6">
@@ -51,6 +66,7 @@ export function Feedbacks() {
                                         <img className="mx-auto mt-1" src="./img/Website---eBacsi---final---cut_17.jpg" alt="." />
                                         <img className="w-[11.938rem] mx-auto mb-[2.3rem] mt-6 rounded-full shadow-2xl border-2 border-white" src={feedback.avatar} alt="customer-avatar" />
                                         <p className="text-gray text-2xl leading-8">{feedback.comment}</p>
+                                        <Rating keys={index} rating={feedback.rating} />
                                     </div>
                                 </div>
                             )
@@ -64,18 +80,18 @@ export function Feedbacks() {
 
 const feedbacks = [
     {
-        avatar: "./img/customers_feedbacks/customer1.jpg",
+        avatar: "./img/feedbacks/customer1.jpg",
         comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
-        rating: "4"
+        rating: "4.5"
     },
     {
-        avatar: "./img/customers_feedbacks/customer2.jpg",
+        avatar: "./img/feedbacks/customer2.jpg",
         comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
         rating: "5"
     },
     {
-        avatar: "./img/customers_feedbacks/customer3.jpg",
+        avatar: "./img/feedbacks/customer3.jpg",
         comment: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis. ",
-        rating: "4.5"
+        rating: "4"
     },
 ]
